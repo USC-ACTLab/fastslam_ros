b296dd71ba429d7e7b3cc61c97a7e40e69a029df
Will Wu <williamwu8-8@outlook.com>
2024-07-10 18:15:30 -0400

    ALP-111: Correct minor mistakes

--- src/create3-fastslam.cpp
+++ src/create3-fastslam.cpp
@@ -25,7 +25,8 @@ static const float x_accel_var = 0.0001543f;
 static const float y_accel_var = 0.0001636f;
 static const float theta_var = 0.0000125f;
 
-constexpr float IMU_UPDATE_FREQ = 1.0f / 100.0f;
+constexpr float IMU_UPDATE_T_s = 1.0f / 100.0f;
+constexpr float IMU_INTEGRATION_s2 = IMU_UPDATE_T_s * IMU_UPDATE_T_s;
 
 static float x_accu = 0.0f;
 static float y_accu = 0.0f;
@@ -101,16 +102,16 @@ void FastSLAMC3::imu_callback(const sensor_msgs::msg::Imu& msg){
   auto a_x = msg.linear_acceleration.x;
   auto a_y = msg.linear_acceleration.y;
   
-  m_rob_pose_delta.x = 0.5 * a_x  * IMU_UPDATE_FREQ;
-  m_rob_pose_delta.y = 0.5 * a_y  * IMU_UPDATE_FREQ;
+  m_rob_pose_delta.x = 0.5 * a_x  * IMU_INTEGRATION_s2;
+  m_rob_pose_delta.y = 0.5 * a_y  * IMU_INTEGRATION_s2;
   m_rob_pose_delta.theta_rad = quaternion_to_yaw(msg.orientation);
 
   RCLCPP_DEBUG(this->get_logger(), "Publishing robot odom:");
   auto message = nav_msgs::msg::Odometry();
   x_accu += m_rob_pose_delta.x;
   y_accu += m_rob_pose_delta.y;
-  message.pose.pose.position.x = x_accu.x;
-  message.pose.pose.position.y = y_accu.y;
+  message.pose.pose.position.x = x_accu;
+  message.pose.pose.position.y = y_accu;
 
   m_deadreckon_odom->publish(message);
 }

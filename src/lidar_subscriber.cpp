d0a7ec176ece9de7a97f06304637d95f39852a08
juliagranato <julia_r_granato@brown.edu>
2024-07-20 22:01:32 +0000

    ALP-110: fixed wrapping issues

--- src/lidar_subscriber.cpp
+++ src/lidar_subscriber.cpp
@@ -11,7 +11,7 @@
 #include <limits>
 #include "visualization_msgs/msg/marker.hpp"
 
-constexpr float LANDMARK_THRESHOLD_m = 0.0125;
+constexpr float LANDMARK_THRESHOLD_m = 0.02;
 constexpr bool testing = true;
 
 bool same_landmark(const Observation2D &first, const Observation2D &second){
@@ -20,21 +20,39 @@ bool same_landmark(const Observation2D &first, const Observation2D &second){
   float theta1 = first.bearing_rad;
   float theta2 = second.bearing_rad;
   float distance = sqrt(pow(r1,2.0)+pow(r2, 2.0)-2*r1*r2*cos(theta1-theta2));
+  if (distance >= LANDMARK_THRESHOLD_m){
+	//std::cout<<distance<<" ";		 
+   }
   return  distance <= LANDMARK_THRESHOLD_m;
 }
 
-Observation2D merge(const Observation2D &existing_landmark, const Observation2D &new_measurement, int &count){
+Observation2D merge(Observation2D &existing_landmark, Observation2D &new_measurement, int &count){
   struct Observation2D average;
   average.range_m = ((count-1)*existing_landmark.range_m+new_measurement.range_m)/count;
+  if(existing_landmark.bearing_rad>M_PI){
+	  existing_landmark.bearing_rad -=2*M_PI;
+  }
+  if(new_measurement.bearing_rad>M_PI){
+	  new_measurement.bearing_rad-=2*M_PI;
+  }
   average.bearing_rad = ((count-1)*existing_landmark.bearing_rad+new_measurement.bearing_rad)/count;
   return average;
 }
 
-Observation2D merge(const Observation2D &first, const Observation2D &second, const int &count1, const int &count2){
+Observation2D merge(Observation2D &first, Observation2D &second, const int &count1, const int &count2){
   //standard weighted average --> between first and last landmark
   struct Observation2D average;
-  average.range_m = first.range_m*(count1/(count1+count2))+second.range_m*(count2/(count1+count2));
-  average.bearing_rad = first.bearing_rad*(count1/(count1+count2))+second.bearing_rad*(count2/(count1+count2));
+  float weight1 = ((float)count1)/((float)(count1+count2));
+  float weight2 = ((float)count2)/((float)(count1+count2));
+  if(first.bearing_rad>M_PI){
+          first.bearing_rad -=2*M_PI;
+  }
+  if(second.bearing_rad>M_PI){
+          second.bearing_rad-=2*M_PI;
+  }
+  average.range_m = first.range_m*weight1+second.range_m*weight2;
+  average.bearing_rad = first.bearing_rad*weight1+(second.bearing_rad)*weight2;
+  //std::cout<<first.bearing_rad<<" " <<second.bearing_rad<<"\n";
   return average;
 }
 
@@ -42,7 +60,7 @@ class LidarSubscriber : public rclcpp::Node
 {
   public:
     LidarSubscriber()
-    : Node("lidar_subscriber")
+   : Node("lidar_subscriber")
     {
       subscription = this->create_subscription<sensor_msgs::msg::LaserScan>(
       "scan_filtered", 10, std::bind(&LidarSubscriber::laserscan_to_landmarks, this, std::placeholders::_1));
@@ -57,6 +75,7 @@ class LidarSubscriber : public rclcpp::Node
         std::queue<Observation2D> landmarks;
         std::queue<int> counts; 
 	struct Observation2D previous_measurement;
+	struct Observation2D first_measurement;
         for (int i = 1; i < measurement_count; i++){
             float curr_range = msg->ranges[i];
             if (curr_range == std::numeric_limits<float>::infinity()||curr_range == -1*std::numeric_limits<float>::infinity()){ 
@@ -67,6 +86,7 @@ class LidarSubscriber : public rclcpp::Node
 	       if (landmarks.empty()){
 		       landmarks.push(curr_measurement);
 		       previous_measurement = curr_measurement;
+		       first_measurement = curr_measurement;
 		       counts.push(1);	       
 	       }else if(same_landmark(curr_measurement, previous_measurement)){
                   counts.back()++;
@@ -80,16 +100,16 @@ class LidarSubscriber : public rclcpp::Node
 	       previous_measurement = curr_measurement;
             }
         }
-
+	struct Observation2D last_measurement = previous_measurement;
         //if the first and last landmarks are actually the same, they get combined!
-	if (landmarks.size()>1 and same_landmark(landmarks.front(), landmarks.back())){
+	if (landmarks.size()>1 and same_landmark(first_measurement, last_measurement)){
           struct Observation2D new_last = merge(landmarks.front(), landmarks.back(), counts.front(), counts.back());
           (landmarks.back()).range_m = new_last.range_m;
           (landmarks.back()).bearing_rad = new_last.bearing_rad; 
           landmarks.pop();
         }
 	
-        std::cout<<landmarks.size()<<"\n";
+std::cout<<landmarks.size()<<" "<<landmarks.front().range_m<<" "<<landmarks.front().bearing_rad<<"\n";
 	if (testing){
 	    std::queue<Observation2D>landmarks_copy(landmarks);
 	     visualize_landmarks(landmarks_copy);
@@ -106,11 +126,13 @@ class LidarSubscriber : public rclcpp::Node
 	 message.ns = "landmark_list";
 	 message.id = 0;
 	 message.pose.orientation.w = 1.0;
-	 message.scale.x = 0.2;
-	 message.scale.y = 0.2;
-	 message.color.r = 3;
-	 message.color.g = 252;
-	 message.color.b = 61;
+	 message.scale.x = 1.0;
+	 message.scale.y = 1.0;
+	 auto color = std_msgs::msg::ColorRGBA();
+	 color.r = 0;
+	 color.g = 0;
+	 color.b = 20;
+	 color.a = 0.8;
 	 while(!landmarks.empty()){
 		auto curr_point = geometry_msgs::msg::Point();
 		Observation2D curr_landmark = landmarks.front();
@@ -118,10 +140,11 @@ class LidarSubscriber : public rclcpp::Node
 		curr_point.y = curr_landmark.range_m*sin(curr_landmark.bearing_rad);
 		message.points.push_back(curr_point);
 		landmarks.pop();
+		message.colors.push_back(color);
 	 }
 	 publisher->publish(message);
-    }
-};
+	 }
+  };
 
 
 int main(int argc, char * argv[])

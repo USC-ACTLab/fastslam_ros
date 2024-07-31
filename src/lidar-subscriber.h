9c236e339abd98da1e65876ddaf6bb3ef78b3274
juliagranato <julia_r_granato@brown.edu>
2024-07-31 19:55:47 +0000

    JG/ALP-130: Properly deleted extra garbage without changing FastSLAM

--- src/lidar-subscriber.h
+++
@@ -1,6 +0,0 @@
-#include <queue>
-#include "core-structs.h"
-#include "sensor_msgs/msg/laser_scan.hpp"
-#include "visualization_msgs/msg/marker.hpp"
-std::queue<Observation2D> laserscan_to_landmarks(const sensor_msgs::msg::LaserScan::SharedPtr msg, rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr observation_visualization_publisher);
-
